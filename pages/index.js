8dc99312b2102d618b487286101a13c16052da0e
Gustavo <gustavovitordeveloper@gmail.com>
2023-12-05 01:28:45 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,9 @@ export async function getServerSideProps(context) {
   const host = context.req.headers['x-forwarded-host'] || context.req.headers.host;
   const subdomain = url.parse(`https://${host}`).hostname.split('.')[0];
 
+  console.log(subdomain);
+  console.log(process.env.NEXT_PUBLIC_BASE_URL_DOMAIN);
+
   if (subdomain != process.env.NEXT_PUBLIC_BASE_URL_DOMAIN) {
     try {
       const response = await fetch(`https://api.edenerp.com.br:8081/home?empresa=${subdomain}`);
